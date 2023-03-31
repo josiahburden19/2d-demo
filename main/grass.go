e66c99d086168da3618e73a72a6bcb0203095f15
syedyus <123729735+syedyus@users.noreply.github.com>
2023-03-31 19:00:04 +0100

    changes

--- main/grass.go
+++ main/grass.go
@@ -9,12 +9,17 @@ embedded_components {
   position {
     x: 0.0
     y: 0.0
-    z: 0.0
+    z: -2.0
   }
   rotation {
     x: 0.0
     y: 0.0
     z: 0.0
     w: 1.0
+  }
+  scale {
+    x: 1.0
+    y: 1.0
+    z: 1.0E-6
   }
 }
